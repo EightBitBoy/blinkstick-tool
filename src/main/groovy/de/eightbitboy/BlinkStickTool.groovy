29a6a12d8d3f118362509138767690822ace1f21
Stefan Härtel <haertel.dev@gmail.com>
2016-09-09 12:01:58 +0200

    Add libraries, stuff works

--- src/main/groovy/de/eightbitboy/BlinkStickTool.groovy
+++ src/main/groovy/de/eightbitboy/BlinkStickTool.groovy
@@ -1,7 +1,12 @@
 package de.eightbitboy
 
+import blinkstick.*
+
 class BlinkStickTool {
 	static void main(String[] args) {
 		println('Hello world!')
+
+		BlinkStick device = BlinkStick.findFirst();
+		println(device)
 	}
 }

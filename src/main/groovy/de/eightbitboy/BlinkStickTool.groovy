3eaafb3291d8e7b5593c254c395e049b1039607c
Stefan Härtel <haertel.dev@gmail.com>
2016-09-09 12:30:41 +0200

    Playing

--- src/main/groovy/de/eightbitboy/BlinkStickTool.groovy
+++ src/main/groovy/de/eightbitboy/BlinkStickTool.groovy
@@ -8,7 +8,19 @@ class BlinkStickTool {
 
 		BlinkStick device = BlinkStick.findFirst();
 		println(device)
-		device.setColor(100, 0, 255)
+
+		int i = 1
+		while(true){
+			sleep(700)
+
+			int prev = (i - 1) % 8
+			int current = i % 8
+			
+			//device.setIndexedColor(0, prev, 0, 0, 0)
+			device.setIndexedColor(0, current, 255, 0, 0)
+
+			i++
+		}
 
 		System.exit(0)
 	}

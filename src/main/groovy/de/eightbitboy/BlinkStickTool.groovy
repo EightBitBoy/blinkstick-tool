dfa7d3ed865d54f78bbfbf183269e0a83004d3f2
Stefan Härtel <haertel.dev@gmail.com>
2016-09-12 07:35:41 +0200

    More lights!

--- src/main/groovy/de/eightbitboy/BlinkStickTool.groovy
+++ src/main/groovy/de/eightbitboy/BlinkStickTool.groovy
@@ -3,6 +3,8 @@ package de.eightbitboy
 import blinkstick.*
 
 class BlinkStickTool {
+	static int PAUSE = 20
+
 	static void main(String[] args) {
 		println('Hello world!')
 
@@ -11,14 +13,33 @@ class BlinkStickTool {
 
 		int i = 1
 		while(true){
-			sleep(70)
-
-			int prev = (i - 1) % 8
-			int current = i % 8
-			
-			device.setIndexedColor(0, prev, 0, 0, 0)
 			sleep(20)
-			device.setIndexedColor(0, current, 255, 0, (i * 4) % 255)
+
+			int index0 = (i + 0) % 8
+			int index1 = (i + 1) % 8
+			int index2 = (i + 2) % 8
+			int index3 = (i + 3) % 8
+			int index4 = (i + 4) % 8
+			int index5 = (i + 5) % 8
+			int index6 = (i + 6) % 8
+			int index7 = (i + 7) % 8
+			
+			device.setIndexedColor(0, index0, (i * 4) % 255, 255, (i * 4) % 255)
+			sleep(PAUSE)
+			device.setIndexedColor(0, index1, 0, 255, (i * 4) % 255)
+			sleep(PAUSE)
+			device.setIndexedColor(0, index2, 255, 0, (i * 4) % 255)
+			sleep(PAUSE)
+			device.setIndexedColor(0, index3, 255, (i * 4) % 255, 0)
+			sleep(PAUSE)
+			device.setIndexedColor(0, index4, 0, (i * 4) % 255, 255)
+			sleep(PAUSE)
+			device.setIndexedColor(0, index5, (i * 4) % 255, 0, 255)
+			sleep(PAUSE)
+			device.setIndexedColor(0, index6, (i * 4) % 255, 255, 0)
+			sleep(PAUSE)
+			device.setIndexedColor(0, index7, (i * 4) % 255, (i * 4) % 255, 255)
+			sleep(PAUSE)
 
 			i++
 			i = i % 255
